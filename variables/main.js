05b75d968e1d69ec43945a1034901ccd3147352d
serviktor050 <mr.s-gagulin@yandex.ru>
2019-06-27 20:33:38 +0300

    Работа выполнена

---
+++ variables/main.js
@@ -0,0 +1,23 @@
+//Задача 1
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+let numberOfSubjects = 3;
+let avergeScore = (algebra + geography + physics)/numberOfSubjects;
+
+console.log(avergeScore);
+
+//Задача 2
+let name = 'Сергей';
+let welcomeMessage = `Привет, мир! Меня зовут ${name}.`;
+
+console.log(welcomeMessage);
+
+//Задача 3
+let x,y,z;
+x = 2;
+y = 22;
+z = 0;
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
